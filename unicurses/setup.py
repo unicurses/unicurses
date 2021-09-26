075bbf7c12ae1e9161d77e0af11694d85014eef7
GiorgosXou <gxousos@gmail.com>
2021-09-26 10:57:46 +0300

    v1.3.7

--- unicurses/setup.py
+++ unicurses/setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup # 1
 root = sys.argv[0].replace("setup.py","")
 
 setup(name='Uni-Curses',
-      version='1.3.6',
+      version='1.3.7',
       description='Unified Curses Wrapper for Python',
       long_description='A universal Curses wrapper for Python on Windows, Linux, and\nMac OS X, with syntax close to the original NCurses. In order\nto provide Curses functionality on Windows it utilizes the ctype\nforeign function interface to wrap PDCurses, a free and open-source\nCurses implementation for Windows. CONTRIBUTORS = [GiorgosXou]',
       author='Michael Kamensky',
@@ -17,10 +17,10 @@ setup(name='Uni-Curses',
       py_modules=['unicurses'],
       license='General Public License v3',
       platforms=['Windows', 'Linux', 'Mac OS X'],
-      data_files=[('Lib/site-packages/unicurses/32 bit binaries/pdc34dll'   , glob(root + '32 bit binaries/pdc34dll/*.*' )),
-                  ('Lib/site-packages/unicurses/32 bit binaries/pdc34dlls'  , glob(root + '32 bit binaries/pdc34dlls/*.*')),
-                  ('Lib/site-packages/unicurses/32 bit binaries/pdc34dllu'  , glob(root + '32 bit binaries/pdc34dllu/*.*')),
-                  ('Lib/site-packages/unicurses/32 bit binaries/pdc34dllw'  , glob(root + '32 bit binaries/pdc34dllw/*.*')),
+      data_files=[('Lib/site-packages/unicurses/32 bit binaries/pdc39dll'   , glob(root + '32 bit binaries/pdc39dll/*.*' )),
+                  ('Lib/site-packages/unicurses/32 bit binaries/pdc39dlls'  , glob(root + '32 bit binaries/pdc39dlls/*.*')),
+                  ('Lib/site-packages/unicurses/32 bit binaries/pdc39dllu'  , glob(root + '32 bit binaries/pdc39dllu/*.*')),
+                  ('Lib/site-packages/unicurses/32 bit binaries/pdc39dllw'  , glob(root + '32 bit binaries/pdc39dllw/*.*')),
 
                   ('Lib/site-packages/unicurses/64 bit binaries/pdc39dll'   , glob(root + '64 bit binaries/pdc39dll/*.*' )),
                   ('Lib/site-packages/unicurses/64 bit binaries/pdc39dlls'  , glob(root + '64 bit binaries/pdc39dlls/*.*')),
